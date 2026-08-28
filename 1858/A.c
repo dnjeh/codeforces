4eaa060efb79129ead18d424e14eb9ed90195995
highend_lilac <ehdnj.001@gmail.com>
2026-08-28 09:12:22 +0900

    Today's Codeforce's

---
+++ 1858/A.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int T, a, b, c;
+    scanf("%d", &T);
+    for(int _T=0;_T<T;_T++) {
+        scanf("%d %d %d", &a, &b, &c);
+        printf("%s\n", a+c%2>b?"First":"Second");
+    }
+    return 0;
+}
